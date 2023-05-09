033d01919d1328b92d2fa09b8ed2f9b95e4759e2
oswaldo <oarincon30@ucatolica.edu.co>
2023-05-09 12:11:56 -0500

    chips cambio color onclick

--- app/src/main/java/com/example/marvelapp/MainActivity.kt
+++ app/src/main/java/com/example/marvelapp/MainActivity.kt
@@ -76,7 +76,134 @@ public class MainActivity : AppCompatActivity() {
             .load(imageUrl10)
             .into(imageView10)
 
+        val chip1 = findViewById<Chip>(R.id.chip1)
+        val chip2 = findViewById<Chip>(R.id.chip2)
+        val chip3 = findViewById<Chip>(R.id.chip3)
+        val chip4 = findViewById<Chip>(R.id.chip4)
+        val chip5 = findViewById<Chip>(R.id.chip5)
+
+        val chip6 = findViewById<Chip>(R.id.chip6)
+        val chip7 = findViewById<Chip>(R.id.chip7)
+        val chip8 = findViewById<Chip>(R.id.chip8)
+        val chip9 = findViewById<Chip>(R.id.chip9)
+        val chip10 = findViewById<Chip>(R.id.chip10)
+
+        var c_state1 = false
+        var c_state2 = false
+        var c_state3 = false
+        var c_state4 = false
+        var c_state5 = false
+
+        var c_state6 = false
+        var c_state7 = false
+        var c_state8 = false
+        var c_state9 = false
+        var c_state10 = false
+
+        chip1.setOnClickListener {
+            if (c_state1 == false) {
+                chip1.setChipBackgroundColorResource(R.color.hero_selected)
+                c_state1 = true
+            } else {
+                chip1.setChipBackgroundColorResource(R.color.hero)
+                c_state1 = false
+            }
+        }
+
+        chip2.setOnClickListener {
+            if (c_state2 == false) {
+                chip2.setChipBackgroundColorResource(R.color.hero_selected)
+                c_state2 = true
+            } else {
+                chip2.setChipBackgroundColorResource(R.color.hero)
+                c_state2 = false
+            }
+        }
+
+        chip3.setOnClickListener {
+            if (c_state3 == false) {
+                chip3.setChipBackgroundColorResource(R.color.hero_selected)
+                c_state3 = true
+            } else {
+                chip3.setChipBackgroundColorResource(R.color.hero)
+                c_state3 = false
+            }
+        }
+
+        chip4.setOnClickListener {
+            if (c_state4 == false) {
+                chip4.setChipBackgroundColorResource(R.color.hero_selected)
+                c_state4 = true
+            } else {
+                chip4.setChipBackgroundColorResource(R.color.hero)
+                c_state4 = false
+            }
+        }
+
+        chip5.setOnClickListener {
+            if (c_state5 == false) {
+                chip5.setChipBackgroundColorResource(R.color.hero_selected)
+                c_state5 = true
+            } else {
+                chip5.setChipBackgroundColorResource(R.color.hero)
+                c_state5 = false
+            }
+        }
+
+        chip6.setOnClickListener {
+            if (c_state6 == false) {
+                chip6.setChipBackgroundColorResource(R.color.villain_selected)
+                c_state6 = true
+            } else {
+                chip6.setChipBackgroundColorResource(R.color.villain)
+                c_state6 = false
+            }
+        }
+
+        chip7.setOnClickListener {
+            if (c_state7 == false) {
+                chip7.setChipBackgroundColorResource(R.color.villain_selected)
+                c_state7 = true
+            } else {
+                chip7.setChipBackgroundColorResource(R.color.villain)
+                c_state7 = false
+            }
+        }
+
+        chip8.setOnClickListener {
+            if (c_state8 == false) {
+                chip8.setChipBackgroundColorResource(R.color.villain_selected)
+                c_state8 = true
+            } else {
+                chip8.setChipBackgroundColorResource(R.color.villain)
+                c_state8 = false
+            }
+        }
+
+        chip9.setOnClickListener {
+            if (c_state9 == false) {
+                chip9.setChipBackgroundColorResource(R.color.villain_selected)
+                c_state9 = true
+            } else {
+                chip9.setChipBackgroundColorResource(R.color.villain)
+                c_state9 = false
+            }
+        }
+
+        chip10.setOnClickListener {
+            if (c_state10 == false) {
+                chip10.setChipBackgroundColorResource(R.color.villain_selected)
+                c_state10 = true
+            } else {
+                chip10.setChipBackgroundColorResource(R.color.villain)
+                c_state10 = false
+            }
+        }
+
+
+
     }
+
 
 
 }
